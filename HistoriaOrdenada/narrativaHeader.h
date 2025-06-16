fca60bd7b33e9d617324294f84afbefe7a258cee
KiyeWolf <guillemirandadev@gmail.com>
2025-06-15 21:25:08 -0300

    Lectura del codigo

--- HistoriaOrdenada/narrativaHeader.h
+++ HistoriaOrdenada/narrativaHeader.h
@@ -10,23 +10,21 @@
 #define ERR_LECTURA_ARCH 1
 #define ACEPTADO 1
 #define RECHAZADO 0
+#define RESPUESTA_CORRECTA 3
+#define CANT_MINIMA_DE_OPCIONES 1
+#define CANT_MAXIMA_DE_OPCIONES 4
+#define ERR_ARCH NULL
 
 
-void HistoriaCompleta();
 char* devolverNombreDeArchSegunCodigo(int codigo_linea);
+
 void leerPreguntaBooleana(int* res);
 int preguntarYLeerSegunTexto(char* texto);
-int tutorial();
-int aceptarAyudarAnimales();
 int cinematica(char* nombreTxt);
-void Altar();
 int acertijo();
 void leerRespuestaMultiples(int* res, int li, int ls);
 
-void aceptarAyudaEsqueleto(int* esqueleto);
-int investigarRuidos();
 int ayudarAVegeta();
-int rodearGolem();
 int perdonarAfreezer();
 
 
