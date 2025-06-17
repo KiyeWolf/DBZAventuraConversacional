cf73c81427ed0ae164042d17c6cfe0e062c29b2f
NicoCaram <nicocaram10@gmail.com>
2025-06-16 22:02:16 -0300

    Item collection actualizado
    Mayor lista de items.

--- Codigo parte 1/item_collection.c
+++ Codigo parte 1/item_collection.c
@@ -8,10 +8,55 @@ t_item item_collection[] =
         0, 0, 0, 0, 0, 0
     },
     {
-        "BOTAS",
-        0,0,0,1,
+        "POSION DE SALUD",
+        20,0,0,0,
         0,0,0,0,0,0
-    }
+    },
+    {
+        "POSION DE PODER",
+        0,20,0,0,
+        0,0,0,0,0,0
+    },
+    {
+        "POSION DE VIGOR",
+        0,0,20,0,
+        0,0,0,0,0,0
+    },
+    {
+        "TRAJE REFORZADO",
+        0,0,0,10,
+        0,0,0,0,0,0
+    },
+    {
+        "GEMA DE SALUD",
+        0,0,0,0,
+        1.2,0,0,0,0,0
+    },
+    {
+        "GEMA DE PODER",
+        0,0,0,0,
+        0,1.2,0,0,0,0
+    },
+    {
+        "GEMA DE VIGOR",
+        0,0,0,0,
+        0,0,1.3,0,0,0
+    },
+    {
+        "TRAJE ENCANTADO",
+        0,0,0,0,
+        0,0,0,1.15,0,0
+    },
+    {
+        "BOTAS ENCANTADAS",
+        0,0,0,0,
+        0,0,0,0,1.3,0
+    },
+    {
+        "BOTAS ANTIGUAS",
+        0,0,0,0,
+        0,0,0,0,0,8
+    },
 };
 
 t_retorno_item devolver_item_por_id(unsigned int id)
--- Codigo parte 1/item_collection.h
+++ Codigo parte 1/item_collection.h
@@ -5,7 +5,16 @@
 
 enum e_items {
     ITEM_VACIO,
-    BOTAS,
+    POSION_DE_SALUD,
+    POSION_DE_PODER,
+    POSION_DE_VIGOR,
+    TRAJE_REFORZADO,
+    GEMA_DE_SALUD,
+    GEMA_DE_PODER,
+    GEMA_DE_VIGOR,
+    TRAJE_ENCANTADO,
+    BOTAS_ENCANTADAS,
+    BOTAS_ANTIGUAS,
     CANTIDAD_DE_ITEMS
 };
 
@@ -18,5 +27,4 @@ typedef struct {
 extern t_item item_collection[];
 
 t_retorno_item devolver_item_por_id(unsigned int id);
-
 #endif // ITEM_COLLECTION_H_INCLUDED
